76508a81f5f6fafda0838bb8638d38586291971c
JeremiasMane <jmane@magnasistemas.com.br>
2021-12-09 19:25:44 -0300

    testes

--- src/br/com/alura/tdd/Calculadora.java
+++ src/br/com/alura/tdd/Calculadora.java
@@ -2,8 +2,21 @@ package br.com.alura.tdd;
 
 public class Calculadora {
 	
+	
 	public int somar (int a, int b) {
 		return a + b;
+	}
+	
+	public int subtrair (int a, int b) {
+		return a - b;
+	}
+	
+	public int didivir (int a, int b) {
+		return a / b;
+	}
+	
+	public int multiplicar (int a, int b) {
+		return a * b;
 	}
 
 }
--- src/br/com/alura/tdd/CalculadoraTestes.java
+++ src/br/com/alura/tdd/CalculadoraTestes.java
@@ -7,11 +7,36 @@ import org.junit.jupiter.api.Test;
 public class CalculadoraTestes {
 	
 	@Test
-	public void deveriaSomarDoisNumerosPositivos() {
+	public void testaSoma() {
 		Calculadora calc = new Calculadora();
-		int soma = calc.somar(3, 7);
+		int soma = calc.somar(4, 6);
 		
 		Assert.assertEquals(10, soma);
+	}
+	
+	@Test
+	public void testaSubrtracao() {
+		Calculadora calc = new Calculadora();
+		int subtrai = calc.subtrair(9, 3);
+		
+		Assert.assertEquals(6, subtrai);
+	}
+	
+	@Test
+	public void testaDivisao() {
+		Calculadora calc = new Calculadora();
+		int didive = calc.didivir(10, 2);
+		
+		Assert.assertEquals(5, didive);
+	}
+	
+	
+	@Test
+	public void testaMultiplicacao() {
+		Calculadora calc = new Calculadora();
+		int multiplica = calc.multiplicar(3, 7);
+		
+		Assert.assertEquals(21, multiplica);
 	}
 	
 }
